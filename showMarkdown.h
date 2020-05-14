a7e3bbdbe643db62c15b76a6e2f81f0f1491847e
oistream <62792133+zxp2019@users.noreply.github.com>
2020-05-14 11:43:42 +0800

    Upload files

---
+++ showMarkdown.h
@@ -0,0 +1,170 @@
+#include <string>
+#include <stack>
+#include <iostream>
+#ifndef MARKDOWN_SHOW_H_
+#define MARKDOWN_SHOW_H_ 
+using namespace std;
+namespace md
+{
+	class mdTool
+	{
+		public:
+			string showMd(string mdCode)
+			{
+//				cout<<mdCode<<endl;
+				int mdCodeLength=mdCode.length();
+				if(mdCodeLength<0)
+				{
+					return "ERROR 201: Error with this program while dividing this line, please tell the developer this problem.";
+				}
+				if(!mdCodeLength)
+				{
+					return "";
+				}
+				string html="";
+				//if header
+				if(mdCode[0]=='#')
+				{
+					//Count the number of '#'s at first.
+					int num=0;
+					for(int i=0;i<mdCodeLength;i++)
+					{
+						if(num>6)
+						{
+							return "ERROR 101: Have more than 6 \'#\'s. Line:[" +mdCode+"]"; 
+						}
+						if(mdCode[i]!='#')
+						{
+							break;
+						}
+						num++;
+					}
+					if(mdCode[num]!=' ')
+					{
+						return "ERROR 102: Expected a space(\' \') after the \'#\'s. Line:["+mdCode+"]";
+					}
+					//Show this line
+					string tmpNum="";
+					tmpNum.push_back(num+'0');
+					return "<h"+tmpNum+">"+showMd(mdCode.substr(num+1,mdCodeLength-num-1))+"</h"+tmpNum+">";
+				}
+				else
+				{
+					for(int i=0;i<mdCodeLength;i++)
+					{
+						//if strong and italic ***
+						if((i+3<mdCodeLength)&&mdCode[i]=='*'&&mdCode[i+1]=='*'&&mdCode[i+2]=='*')
+						{
+							int endPos=-1;
+							for(int j=i+3;j<mdCodeLength-2;j++)
+							{
+								if(mdCode[j]=='*'&& mdCode[j+1]=='*'&&mdCode[j+2]=='*')
+								{
+									endPos=j;
+									break;
+								}
+							}
+							if(endPos==-1)
+							{
+								return "ERROR 106: The \'***\' is not in a pair. Line:["+mdCode+"]";
+							}
+							return mdCode.substr(0,i)+"<b><i>"+showMd(mdCode.substr(i+3,endPos-i-3))+"</b></i>"+showMd(mdCode.substr(endPos+3,mdCodeLength-endPos-3));
+						}
+						//if strong and italic ___
+						if((i+3<mdCodeLength)&&mdCode[i]=='_'&&mdCode[i+1]=='_'&&mdCode[i+2]=='_')
+						{
+							int endPos=-1;
+							for(int j=i+3;j<mdCodeLength-2;j++)
+							{
+								if(mdCode[j]=='_'&& mdCode[j+1]=='_'&&mdCode[j+2]=='_')
+								{
+									endPos=j;
+									break;
+								}
+							}
+							if(endPos==-1)
+							{
+								return "ERROR 108: The \'___\' is not in a pair. Line:["+mdCode+"]";
+							}
+							return mdCode.substr(0,i)+"<b><i>"+showMd(mdCode.substr(i+3,endPos-i-3))+"</b></i>"+showMd(mdCode.substr(endPos+3,mdCodeLength-endPos-3));
+						}
+						//if strong **
+						if((i+1<mdCodeLength)&&mdCode[i]=='*'&&mdCode[i+1]=='*')
+						{
+							int endPos=-1;
+							for(int j=i+2;j<mdCodeLength-1;j++)
+							{
+								if(mdCode[j]=='*'&& mdCode[j+1]=='*')
+								{
+									endPos=j;
+									break;
+								}
+							}
+							if(endPos==-1)
+							{
+								return "ERROR 103: The \'**\' is not in a pair. Line:["+mdCode+"]";
+							}
+							return mdCode.substr(0,i)+"<b>"+showMd(mdCode.substr(i+2,endPos-i-2))+"</b>"+showMd(mdCode.substr(endPos+2,mdCodeLength-endPos-2));
+						}
+						//if strong __
+						if((i+1<mdCodeLength)&&mdCode[i]=='_'&&mdCode[i+1]=='_')
+						{
+							int endPos=-1;
+							for(int j=i+2;j<mdCodeLength-1;j++)
+							{
+								if(mdCode[j]=='_'&& mdCode[j+1]=='_')
+								{
+									endPos=j;
+									break;
+								}
+							}
+							if(endPos==-1)
+							{
+								return "ERROR 104: The \'__\' is not in a pair. Line:["+mdCode+"]";
+							}
+							return mdCode.substr(0,i)+"<b>"+showMd(mdCode.substr(i+2,endPos-i-2))+"</b>"+showMd(mdCode.substr(endPos+2,mdCodeLength-endPos-2));
+						}
+						//if italic *
+						if(mdCode[i]=='*')
+						{
+							int endPos=-1;
+							for(int j=i+1;j<mdCodeLength;j++)
+							{
+								if(mdCode[j]=='*')
+								{
+									endPos=j;
+									break;
+								}
+							}
+							if(endPos==-1)
+							{
+								return "ERROR 105: The \'*\' is not in a pair. Line:["+mdCode+"]";
+							}
+							return mdCode.substr(0,i)+"<i>"+showMd(mdCode.substr(i+1,endPos-i-1))+"</i>"+showMd(mdCode.substr(endPos+1,mdCodeLength-endPos-1));
+						}
+						//if italic _
+						if(mdCode[i]=='_')
+						{
+							int endPos=-1;
+							for(int j=i+1;j<mdCodeLength;j++)
+							{
+								if(mdCode[j]=='_')
+								{
+									endPos=j;
+									break;
+								}
+							}
+							if(endPos==-1)
+							{
+								return "ERROR 107: The \'_\' is not in a pair. Line:["+mdCode+"]";
+							}
+							return mdCode.substr(0,i)+"<i>"+showMd(mdCode.substr(i+1,endPos-i-1))+"</i>"+showMd(mdCode.substr(endPos+1,mdCodeLength-endPos-1));
+						}
+					 } 
+					return mdCode;
+				}
+			}
+			
+	};
+ } 
+#endif
